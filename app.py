b4dfcc8492126c2f4739b26bb60c2435898743fd
Sreedeep CV <sreedeep200@gmail.com>
2023-08-23 21:59:58 +0530

    Added a new fix

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def hello():
         height_inch = cm_to_inches(float(height))
         weight = weightPredict.weigthPredict([[float(height_inch)]])
         return render_template("index.html" , height = height ,inches = height_inch ,weight=weight)
-    return render_template("index.html" , height = height ,inches = height_inch ,weight=weight)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run()
